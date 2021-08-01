0d8cbabaec5e58dacf3cec850ef6b1f3d835a8c8
Cinofe <jeraru5178@gmail.com>
2021-08-01 23:47:39 +0900

    Snake

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,15 +1,17 @@
 import numpy
 import scipy.special
+import random
 
 
 #neural network class definition
 class neuralNetwork:
     
     #initialize the neural network
-    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+    def __init__(self, inputnodes, hidden1, hidden2, outputnodes, learningrate):
         #set number of nodes in each input, hidden, output layer 
         self.inodes = inputnodes
-        self.hnodes = hiddennodes
+        self.hnode1 = hidden1
+        self.hnode2 = hidden2
         self.onodes = outputnodes
 
         #가중치 행렬 wih(input_hidden)와 who(hidden_output)
@@ -17,30 +19,47 @@ class neuralNetwork:
         #w11 w21
         #w12 w22
 
-        self.wih = numpy.random.normal(0.0, pow(self.hnodes, - 0.5),(self.hnodes, self.inodes))
-        self.who = numpy.random.normal(0.0, pow(self.onodes, - 0.5),(self.onodes, self.hnodes))
+        self.wih = numpy.random.normal(0.0, pow(self.hnode1, - 0.5),(self.hnode1, self.inodes))
+        self.whh = numpy.random.normal(0.0, pow(self.hnode2, - 0.5),(self.hnode2, self.hnode1))
+        self.who = numpy.random.normal(0.0, pow(self.onodes, - 0.5),(self.onodes, self.hnode2))
 
         #learning rate
         self.lr = learningrate
 
-        #expit()는 시그모이드 함수
-        self.activation_function = lambda x: scipy.special.expit(x) 
+        #시그모이드 활성화 함수
+        self.activation_function = lambda x: scipy.special.expit(x)
+        #Relu 활성화 함수
+        self.Relu_func = lambda x : numpy.maximum(0,x)
+        #softmax 활성화 함수
+        self.softmax_func = lambda x: self.softmax(x)
+
+    def softmax(self, x):
+        c = numpy.max(x)
+        exp_a = numpy.exp(x-c)
+        sum_exp_a = numpy.sum(exp_a)
+        y = exp_a / sum_exp_a
+
+        return y
 
     #신경망 학습시키기
-    def train():
+    def train(self):
         pass
 
     #신경망에 질의하기
     def query(self, inputs_list):
       #입력 리스트를 2차원 행렬로 변환
       inputs = numpy.array(inputs_list, ndmin=2).T
-      #은닉 계층으로 들어오는 신호를 계산
-      hidden_inputs = numpy.dot(self.wih, inputs)
-      #은닉 계층에서 나가는 신호를 계산
-      hidden_outputs = self.activation_function(hidden_inputs)
+      #1차 은닉 계층으로 들어오는 신호를 계산
+      hidden1_inputs = numpy.dot(self.wih, inputs)
+      #1차 은닉 계층에서 나가는 신호를 계산
+      hidden1_outputs = self.activation_function(hidden1_inputs)
+      #2차 은닉 계층으로 들어오는 신호를 계산
+      hidden2_inputs = numpy.dot(self.whh,hidden1_outputs)
+      #2차 은닉 계층에서 나가는 신호를 계산
+      hidden2_outputs = self.Relu_func(hidden2_inputs)
       #최종 출력 계층으로 들어오는 신호 계산 
-      final_inputs = numpy.dot(self.who, hidden_outputs)
+      final_inputs = numpy.dot(self.who, hidden2_outputs)
       #최종 출력 계층에서 나가는 신호 계산 
-      final_outputs = self.activation_function(final_inputs)
-      
+      final_outputs = numpy.round(self.softmax_func(final_inputs),15)
+
       return final_outputs
--- Snake.py
+++ Snake.py
@@ -8,64 +8,74 @@ import math
 from pygame.constants import NOEVENT
 import NeuralNet as NL
 
-#pygame 초기화
-pygame.init()
-
-#화면 크기
-size = [850,668]
-
-#배경색
-BLACK = (0,0,0)
-#snake색
-WHITE = (255,255,255)
-#apple색
-RED = (255,0,0)
-
-#배경 설정
-screen = pygame.display.set_mode(size)
-
-#시간 관련 변수
-clock = pygame.time.Clock()
-last_moved_time = datetime.now()
-
-#input layer
-input_layers = list()
-distan_to_apple = list()
-distan_to_body = list()
-distan_to_wall = list()
-
-#게임 종료 체크 변수
-done = False
-
-#점수
-score = 0
-score_font = pygame.font.SysFont('OpenSans', 30)
-
-KEY_DIRECTION = {
-    pygame.K_UP: 'U',
-    pygame.K_DOWN: 'D',
-    pygame.K_LEFT: 'L',
-    pygame.K_RIGHT: 'R'
-}
-
-KEY_OUTPUT = {
-    0:"U",
-    1:"D",
-    2:"L",
-    3:"R"
-}
-
+#게임 시스템
 class Game_System:
+
+    def __init__(self):
+        #게임 종료 체크 변수
+        self.done = False
+        #점수
+        self.score = 0
+        self.best_score = 0
+        self.best_score_weight = list()
+        #폰트
+        self.font = pygame.font.SysFont('OpenSans', 30)
+        self.font_exit = pygame.font.SysFont('OpenSans',25)
+        #재시작 횟수
+        self.count = 0
+        #무한 반복 방지 제약 생명
+        self.life = 1000
+        #시간 관련 변수
+        self.last_moved_time = datetime.now()
+        #배경색
+        self.BLACK = (0,0,0)
+        #snake색
+        self.WHITE = (255,255,255)
+        #apple색
+        self.RED = (255,0,0)
+        #화면 설정
+        self.screen = pygame.display.set_mode([850,668])
+        self.clock = pygame.time.Clock()
+        #키보드 입력값 딕셔너리
+        self.KEY_DIRECTION = {
+            pygame.K_UP: 'U',
+            pygame.K_DOWN: 'D',
+            pygame.K_LEFT: 'L',
+            pygame.K_RIGHT: 'R',
+            pygame.K_ESCAPE: 'Q'
+        }
+        #신경망 출력값 딕셔너리
+        self.KEY_OUTPUT = {
+            0:"U",
+            1:"D",
+            2:"L",
+            3:"R"
+        }
+
      
     def draw_block(self, screen, color, position):
         block = pygame.Rect((position[0], position[1]), (20, 20))
         pygame.draw.rect(screen, color, block)
+    
+    def gameover(self,mode=None):
+        time.sleep(1)
+        self.count += 1
+        self.life = 1000
+        self.score = 0
+        self.done = False
+
+        runGame(mode)  
+    
+    
+
+
 
 
 class Snake(Game_System):
     
     def __init__(self):
-        self.positions = [(48,50),(70,50),(92,50)]
+        super().__init__()
+        self.positions = [(4+308,6+308),(26+308,6+308),(48+308,6+308)]
         self.direction = "D"
         self.distnaceApple = []
         self.distnaceWall = []
@@ -73,7 +83,7 @@ class Snake(Game_System):
 
     def draw(self):
         for position in self.positions:
-            super().draw_block(screen, WHITE, position)
+            self.draw_block(self.screen, self.WHITE, position)
     
     def move(self):
         head_positon = self.positions[0]
@@ -171,89 +181,105 @@ class Snake(Game_System):
 
 class Apple(Game_System):
     def __init__(self,position = ((random.randint(0,30)*22)+4,(random.randint(0,30)*22)+6)):
+        super().__init__()
         self.position = position
     def draw(self):
-        super().draw_block(screen,RED,self.position)
+        self.draw_block(self.screen,self.RED,self.position)
 
-snake = Snake()
-apple = Apple()
-
-output = None
-key = None
-
-
-while not done:
-    clock.tick(60)
-    screen.fill(BLACK)
-
-    snake_head = snake.positions[0]
-
-    #테두리
-    pygame.draw.rect(screen,WHITE,[0,2,664,666],2)
-
-    #점수 표시
-    screen.blit(score_font.render("score : "+str(score),False,WHITE),(700,40))
-
-    #input layer 값 찾기
-    #head와 사과 까지의 거리
-
-    brain = NL.neuralNetwork(24,12,4,0.5)
-
+def runGame(mode = None):
     
-    if output != None:
-        print(max(output))
-        key = output.index(max(output))
+    pygame.init()
 
-    if (snake_head in snake.positions[1:]):
-        break
+    sys = Game_System()
+    snake = Snake()
+    apple = Apple()
 
-    for event in pygame.event.get():
-        #게임 종료
-        if event.type == pygame.QUIT:
-            done=True
-        #키보드 조작시 사용
-        if event.type == pygame.KEYDOWN:
-            if event.key in KEY_DIRECTION:
-                if KEY_DIRECTION[event.key] == "U" and snake.direction != 'D':
-                    snake.direction = KEY_DIRECTION[event.key]
-                elif KEY_DIRECTION[event.key] == "D" and snake.direction != 'U':
-                    snake.direction = KEY_DIRECTION[event.key]
-                elif KEY_DIRECTION[event.key] == "L" and snake.direction != 'R':
-                    snake.direction = KEY_DIRECTION[event.key]
-                elif KEY_DIRECTION[event.key] == "R" and snake.direction != 'L':
-                    snake.direction = KEY_DIRECTION[event.key]
+    output = None
+    key = None
 
-        if key != None:
-            print(key)
-            if KEY_OUTPUT[key] == "U" and snake.direction != "D":
-                snake.direction = KEY_OUTPUT[key]
-            elif KEY_OUTPUT[key] == "D" and snake.direction != "U":
-                snake.direction = KEY_OUTPUT[key]
-            elif KEY_OUTPUT[key] == "L" and snake.direction != "R":
-                snake.direction = KEY_OUTPUT[key]
-            elif KEY_OUTPUT[key] == "R" and snake.direction != "L":
-                snake.direction = KEY_OUTPUT[key]
-    
-    if timedelta(seconds=0.075) <= datetime.now() - last_moved_time:
-        snake.move()
-        snake.apple_Distance(apple.position)
-        snake.wall_Distance()
-        snake.body_Distance()
-        if len(snake.distnaceApple+snake.distnaceWall+snake.distanceBody) == 24:
-            output = list(brain.query(snake.distnaceApple+snake.distnaceWall+snake.distanceBody))
-        last_moved_time = datetime.now()
+    brain = NL.neuralNetwork(24,12,12,4,0.5)
 
-    if snake_head == apple.position:
-        score += 1  
-        snake.grow()
-        while True:
-            new_position = ((random.randint(0,30)*22)+4,(random.randint(0,30)*22)+6)
-            if not(new_position in snake.positions) and (new_position[0] >= 4 and new_position[0] < 664) and (new_position[1] >= 6 and new_position[1] < 666):
-                apple.position = new_position
-                break
-    if (snake_head[0] <= -1 or snake_head[0] >= 644) or (snake_head[1] <= -1 or snake_head[1] >= 646):
-        break
+    while not sys.done:
+        sys.clock.tick(60)
+        sys.screen.fill(sys.BLACK)
 
-    snake.draw()
-    apple.draw()
-    pygame.display.flip()
+        snake_head = snake.positions[0]
+
+        #테두리
+        pygame.draw.rect(sys.screen,sys.WHITE,[0,2,664,666],2)
+
+        #점수 표시
+        sys.screen.blit(sys.font.render("score : "+str(sys.score),False,sys.WHITE),(700,40))
+        sys.screen.blit(sys.font.render("life : "+str(sys.life),False,sys.WHITE),(700,80))
+        sys.screen.blit(sys.font.render("count : "+str(sys.count),False,sys.WHITE),(700,120))
+        sys.screen.blit(sys.font_exit.render("Press ESC to EXIT",False,sys.WHITE),(685,600))
+
+        if output != None:
+            key = output.index(max(output))
+
+        if (snake_head in snake.positions[1:]):
+            sys.gameover(mode)
+            break
+
+        for event in pygame.event.get():
+            #게임 종료
+            if event.type == pygame.QUIT:
+                sys.done=True
+            #키보드 조작시 사용
+            if mode != None:
+                if event.type == pygame.KEYDOWN:
+                    if event.key in sys.KEY_DIRECTION:
+                        if sys.KEY_DIRECTION[event.key] == "Q":
+                            exit()
+                        if sys.KEY_DIRECTION[event.key] == "U" and snake.direction != 'D':
+                            snake.direction = sys.KEY_DIRECTION[event.key]
+                        elif sys.KEY_DIRECTION[event.key] == "D" and snake.direction != 'U':
+                            snake.direction = sys.KEY_DIRECTION[event.key]
+                        elif sys.KEY_DIRECTION[event.key] == "L" and snake.direction != 'R':
+                            snake.direction = sys.KEY_DIRECTION[event.key]
+                        elif sys.KEY_DIRECTION[event.key] == "R" and snake.direction != 'L':
+                            snake.direction = sys.KEY_DIRECTION[event.key]
+            else :
+                if key != None and snake.direction != sys.KEY_OUTPUT[key]:
+                    if sys.KEY_OUTPUT[key] == "U" and snake.direction != "D":
+                        snake.direction = sys.KEY_OUTPUT[key]
+                    elif sys.KEY_OUTPUT[key] == "D" and snake.direction != "U":
+                        snake.direction = sys.KEY_OUTPUT[key]
+                    elif sys.KEY_OUTPUT[key] == "L" and snake.direction != "R":
+                        snake.direction = sys.KEY_OUTPUT[key]
+                    elif sys.KEY_OUTPUT[key] == "R" and snake.direction != "L":
+                        snake.direction = sys.KEY_OUTPUT[key]
+        
+        if timedelta(seconds=0.075) <= datetime.now() - sys.last_moved_time:
+            snake.move()
+            snake.apple_Distance(apple.position)
+            snake.wall_Distance()
+            snake.body_Distance()
+            if len(snake.distnaceApple+snake.distnaceWall+snake.distanceBody) == 24:
+                output = list(brain.query(snake.distnaceApple+snake.distnaceWall+snake.distanceBody))
+            sys.last_moved_time = datetime.now()
+
+        if snake_head == apple.position:
+            sys.score += 1  
+            snake.grow()
+            while True:
+                new_position = ((random.randint(0,30)*22)+4,(random.randint(0,30)*22)+6)
+                if not(new_position in snake.positions) and (new_position[0] >= 4 and new_position[0] < 664) and (new_position[1] >= 6 and new_position[1] < 666):
+                    apple.position = new_position
+                    break
+        if (snake_head[0] <= -1 or snake_head[0] >= 644) or (snake_head[1] <= -1 or snake_head[1] >= 646):
+            sys.gameover(mode)
+            break
+
+        snake.draw()
+        apple.draw()
+        pygame.display.flip()
+
+        if sys.life <= 0:
+            sys.gameover(mode)
+            break
+        else : sys.life -= 1
+
+
+if __name__ == "__main__":
+    runGame(1)
