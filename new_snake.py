74839ab342451260834f77e78bd84ab8ec3d20ef
Cinofe <jeraru5178@gmail.com>
2021-08-11 00:06:34 +0900

    Snake

--- new_snake.py
+++ new_snake.py
@@ -178,6 +178,7 @@ class Food:
     def __init__(self):
         #색(빨강)
         self.foodColor = (255,0,0)
+        #매번 좌표는 랜덤으로 부여
         self.pos = ((random.randint(0,29)*22)+4,(random.randint(0,29)*22)+6)
 
 if __name__ == "__main__":
